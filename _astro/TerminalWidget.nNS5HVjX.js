61facf4a173ef9c1c0287bab46aa90d1f3bb3a34
kaida <kaida.palooza@gmail.com>
2026-03-14 03:29:13 +0700

    Updates

--- _astro/TerminalWidget.nNS5HVjX.js
+++ _astro/TerminalWidget.nNS5HVjX.js
@@ -1,4 +1,4 @@
-import{d as i,y as h,q as x}from"./hooks.module.BVG3ayJl.js";import{u as t}from"./jsxRuntime.module.kJjLVEAC.js";import"./preact.module.EGlgVV74.js";const w=`<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+import{d as i,y as h,q as x}from"./hooks.module.CZs5MtpG.js";import{u as t}from"./jsxRuntime.module.wmpUa1Ri.js";import"./preact.module.BytUPcf3.js";const w=`<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
   <rect x="9" y="9" width="13" height="13" rx="2"/>
   <path d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1"/>
 </svg>
